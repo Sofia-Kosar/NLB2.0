daa2cdf7965020b22b0544888f2b09841e145903
Sofia-kosar <Sofiia.Kosar.RI.2023@lpnu.ua>
2023-09-26 19:53:25 +0300

    new

--- NLB2.0/nlb2.0.cpp
+++ NLB2.0/nlb2.0.cpp
@@ -11,7 +11,7 @@ int main()
 	double a; // вхідний параметр
 	double Pi = 4 * atan(1.); // число пі
 	double z1; // результат обчислення 1-го виразу
-	double z2; // результат обчислення 2-го виразу
+	// double z2; // результат обчислення 2-го виразу
 
 
 
@@ -21,10 +21,10 @@ int main()
 	z1 = (sin((Pi / 2) + 3 * a)) / (1 - sin(3 * a + Pi));
 
 
-	z2 = cos((5 * Pi / 4) + 3 * a / 2) / sin((5 * Pi / 4) + 3 * a / 2);
+	// z2 = cos((5 * Pi / 4) + 3 * a / 2) / sin((5 * Pi / 4) + 3 * a / 2);
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	// cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
 }
